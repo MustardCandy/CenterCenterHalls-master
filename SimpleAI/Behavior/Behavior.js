4eeb5d586d07021248d6ee2b585b31a1a8e72cc6
FuseRush <47150589+FuseRush@users.noreply.github.com>
2019-05-20 10:54:52 -0400

    work at home

--- SimpleAI/Behavior/Behavior.js
+++ SimpleAI/Behavior/Behavior.js
@@ -2,7 +2,7 @@ class Move {
   //cannot always go diagonal but always goes diagonal first
   //only return the step
   static _makePath(cordsA, cordsB){
-
+    
   }
 
   //only checks if its open
@@ -12,7 +12,7 @@ class Move {
 
   //call the other two static functions
   static update(cordsA, cordsB, dungeon){
-    _makePath();
-    _getMove();
+    _makePath(cordsA, cordsB);
+    _getMove(cords, path);
   }
 }
