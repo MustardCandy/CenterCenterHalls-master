61c2a27dd741ee5cc2fb07f0d8474f588f652268
Pizaman15 <47151158+Pizaman15@users.noreply.github.com>
2019-05-20 10:24:14 -0400

    kade said his mom is hot

--- SimpleAI/Behavior/Behavior.js
+++ SimpleAI/Behavior/Behavior.js
@@ -1,14 +1,19 @@
 class simpleAi(){
 
+  //need hypotinuse
+  //connot always go diagonal
+  // always go diagonal first
+  //only return the next step
   static _makePath(){
-
   }
 
+// only has to check if its open
   static getMove(){
 
   }
 
+//calls the other two functions
   static update(){
-    
+
   }
 }
