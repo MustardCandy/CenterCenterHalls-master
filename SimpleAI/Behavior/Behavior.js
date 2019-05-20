fb3f1f3e036a7194109b919d9d857dd13cc60a6f
DakotaAyer <47248767+DakotaAyer@users.noreply.github.com>
2019-05-20 10:52:51 -0400

    .

--- SimpleAI/Behavior/Behavior.js
+++ SimpleAI/Behavior/Behavior.js
@@ -1,15 +1,16 @@
 class simpleAi{
-
   static _makePath(){
 
   }
 
   static getMove(cords, path){
-// Get Move should return true or false if open
+    // Get Move should return true or false if open
+
   }
 
   static update(cordsA, cordsB, dungeon){
-// Update should move the monster from one cell to another
+    // Update should move the monster from one cell to another
+
   }
 }
 
