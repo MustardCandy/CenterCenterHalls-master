a8b1db43f298babb43489baf0cf2997c8b285f1d
Pizaman15 <47151158+Pizaman15@users.noreply.github.com>
2019-05-20 10:28:15 -0400

    starting the static functions

--- SimpleAI/Behavior/Behavior.js
+++ SimpleAI/Behavior/Behavior.js
@@ -1,19 +1,20 @@
 class simpleAi(){
-
   //need hypotinuse
   //connot always go diagonal
   // always go diagonal first
   //only return the next step
   static _makePath(){
+
   }
 
 // only has to check if its open
-  static getMove(){
+  static getMove(cords, path){
 
   }
 
 //calls the other two functions
-  static update(){
+  static update(cordsA, cordsB, dungeon){
 
   }
+
 }
