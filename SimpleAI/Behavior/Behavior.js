649ada23c51b1fc0ac6ac3928ac122f52843bd81
MustardCandy <47151229+MustardCandy@users.noreply.github.com>
2019-05-22 09:17:16 -0400

    Updated stuff

--- SimpleAI/Behavior/Behavior.js
+++ SimpleAI/Behavior/Behavior.js
@@ -1,44 +1,20 @@
-
-class aiBehavior{
-
-
-  static _makePath(){
-    
+class SimpleAI {
+  //cannot always go diagonal but always goes diagonal first
+  //only return the step
+  static _makePath(cordsA, cordsB){
+    Utils.shift(cordsA - cordsB);
   }
 
-  static _getMove(){
-
+  //only checks if its open
+  static _getMove(cords, path){
+    if (dungeon.map.cell.open()) {
+      return true;
+    }
   }
 
-
-  static _update(){
-    _makepath();
-    _getMove();
+  //call the other two static functions
+  static update(cordsA, cordsB, dungeon){
+    _makePath(cordsA, cordsB);
+    _getMove(cords, path);
   }
-
-
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
