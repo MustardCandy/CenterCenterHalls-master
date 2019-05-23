0ba33e9e296972c81c259f17c28416cad6ebc9d5
Pizaman15 <47151158+Pizaman15@users.noreply.github.com>
2019-05-23 10:26:10 -0400

    getting the last update function to work

--- SimpleAI/Behavior/Behavior.js
+++ SimpleAI/Behavior/Behavior.js
@@ -1,20 +1,37 @@
-class simpleAi(){
+class simpleAi {
   //need hypotinuse
   //connot always go diagonal
-  // always go diagonal first
+  //always go diagonal first
   //only return the next step
-  static _makePath(){
+  static _makePath(cordsA, cordsB){
+    var herocords = cordsB;
+    var monstercords = cordsA;
+    var loco = undefined;
+    var bestValue = 1000000;
+    var nums = Utils.coordinateMorphs();
+     for (var i = 1; i < nums.length; i++) {
+       var tst = {x:nums[i].x + monstercords.x, y:nums[i].y + monstercords.y}
+       var distx = Math.abs(tst.x - monstercords.x);
+       var disty = Math.abs(tst.y - monstercords.y);
+       var newValue = distx + disty;
+       if(newValue < bestValue){
+         bestValue = newValue;
+         loco = nums[i];
+       }
+     }
 
+    return {x:monstercords.x + loco.x, y:monstercords.y + loco.y};
   }
 
 // only has to check if its open
-  static getMove(cords, path){
-    
+  static _getMove(cords, dungeon){
+   return dungeon.map.cell[cords.x][cords.y].open;
   }
 
 //calls the other two functions
-  static update(cordsA, cordsB, dungeon){
-
+  static update(cordsA, cordsB, cords, dungeon){
+    _makePath(cordsA, cordsB);
+    _getMove(cords, path, dungeon);
+     updates.innerHTML = "aidan is a big dumm";
   }
-
 }
