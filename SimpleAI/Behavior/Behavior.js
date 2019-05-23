bf36c9db1a6ac82deb1788e488d2cf57f7ee7548
FuseRush <47150589+FuseRush@users.noreply.github.com>
2019-05-23 10:54:09 -0400

    remove

--- SimpleAI/Behavior/Behavior.js
+++ SimpleAI/Behavior/Behavior.js
@@ -2,13 +2,23 @@ class SimpleAI {
   //cannot always go diagonal but always goes diagonal first
   //only return the step
   static _makePath(start, end){
-    var move = {x: Utils.shift(dungeon.hero.location.x - start.x), y: Utils.shift(dungeon.hero.location.y - start.y)}
+    var move = {
+      x: Utils.shift(dungeon.hero.location.x - start.x),
+      y: Utils.shift(dungeon.hero.location.y - start.y)
+    }
     return {x: start.x + move.x, y: start.y + move.x}
   }
 
   //only checks if its open
   static _getMove(cords, dungeon){
     return dungeon.map.cell[cords.y][cords.x].open;
+  }
+
+  static remove(monster){
+    monster = Utils.typeCheck(monster, "str", "Behavior.js.remove")
+    for (var i = 0; i < cell.inventory.length; i++) {
+      if (cell.inventory[i].name === monster) { return cell.inventory.splice(i)[0]; }
+    }
   }
 
   //call the other two static functions
