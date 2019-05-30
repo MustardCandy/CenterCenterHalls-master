166c1fdd38736ad2de4ba24156969b09c58d0bfd
MustardCandy <47151229+MustardCandy@users.noreply.github.com>
2019-05-30 10:52:46 -0400

    end of class push

--- SimpleAI/Behavior/Behavior.js
+++ SimpleAI/Behavior/Behavior.js
@@ -10,18 +10,31 @@ class SimpleAI {
     return move;
   }
   //only checks if its open
-  static _getMove(aiCoords){
-    if (dungeon.map.cell.open) {
+  static _getMove(nextMove){
+    if (dungeon.map.map[nextMove.x][nextMove.y].open) {
       return true;
     }else {return false}
   }
 
   //call the other two static functions
   static update(heroCoords, aiCoords, dungeon){
-    var tim = new Monster("+","tim",1,{min:1,max:2},"meh");
-    this._makePath(heroCoords, aiCoords);
-    this._getMove(aiCoords);
-    dungeon.map.map[aiCoords.x][aiCoords.y].add(tim);
+      var move = this._makePath(heroCoords,aiCoords);
+      var tim = new Monster("+","KoolaidMan",1,{min:1,max:2},"tim");
+      this._makePath(heroCoords, aiCoords);
+      var nextMoveX = (aiCoords.x + move.x);
+      var nextMoveY = (aiCoords.y + move.y);
+      var nextMove = {x:nextMoveX,y:nextMoveY}
+      this._getMove(aiCoords,nextMove);
+      if(this._getMove(aiCoords, nextMove) == true){
+      console.log(nextMove)
+      dungeon.map.cell[aiCoords.y][aiCoords.x].remove("KoolaidMan")
+      aiCoords.x = nextMove.x;
+      aiCoords.y = nextMove.y;
+      dungeon.map.map[aiCoords.y][aiCoords.x].add(tim);
+      dungeon.map.cell[aiCoords.y][aiCoords.x].remove("tim")
+      dungeon.map.map[aiCoords.y][aiCoords.x].add(tim);
+      drawMap();
+  }
   }
 
 }
