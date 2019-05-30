230d422342cdcc95680eb82143a8a363df373699
MustardCandy <47151229+MustardCandy@users.noreply.github.com>
2019-05-30 09:46:33 -0400

    Memphis fix

--- SimpleAI/Behavior/Behavior.js
+++ SimpleAI/Behavior/Behavior.js
@@ -1,24 +1,27 @@
 class SimpleAI {
   //cannot always go diagonal but always goes diagonal first
   //only return the step
-  static _makePath(cordsA, cordsB){
+  static _makePath(heroCoords, aiCoords){
 
-    var moveX = Utils.shift(cordsA.x - cordsB.x);
-    var moveY = Utils.shift(cordsA.y - cordsB.y);
+    var moveX = Utils.shift(heroCoords.x - aiCoords.x);
+    var moveY = Utils.shift(heroCoords.y - aiCoords.y);
     var move = {x:moveX,y:moveY}
 
     return move;
   }
   //only checks if its open
-  static _getMove(cords){
+  static _getMove(aiCoords){
     if (dungeon.map.cell.open) {
       return true;
     }else {return false}
   }
 
   //call the other two static functions
-  static update(cordsA, cordsB, dungeon){
-    _makePath(cordsA, cordsB);
-    _getMove(cords, path);
+  static update(heroCoords, aiCoords, dungeon){
+    var tim = new Monster("+","tim",1,{min:1,max:2},"meh");
+    this._makePath(heroCoords, aiCoords);
+    this._getMove(aiCoords);
+    dungeon.map.map[aiCoords.x][aiCoords.y].add(tim);
   }
+
 }
