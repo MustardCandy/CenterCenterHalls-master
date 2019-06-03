67d492ba4c0d0b61959b8f62f37fa7efd95facc4
FuseRush <47150589+FuseRush@users.noreply.github.com>
2019-06-03 10:52:57 -0400

    idk if its good but full send or no send

--- BlindAI/Behavior/Behavior.js
+++ BlindAI/Behavior/Behavior.js
@@ -1,10 +1,11 @@
 class BlindAI extends SimpleAI{
   static _chase(start, dungeon, cords){
-    this._update(start, dungeon);
-    if (this._update(start, dungeon) == false && this._getMove(cords, dungeon) == true) {
+    if (this._update(start, dungeon) == false) {
       //use the a^2 + b^2 = c^2 method
+      return Math.sqrt(Math.pow(start, 2) + Math.pow(dungeon, 2));
     } else {
       //use the a^2 + b^2 = c^2 method but take lowest value
+      
     }
   }
 }
