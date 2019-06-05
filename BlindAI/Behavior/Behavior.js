9edd8f1c4d5e3856bb7f0f6e68a4312e41d0d754
FuseRush <47150589+FuseRush@users.noreply.github.com>
2019-06-05 10:52:56 -0400

    almost done, need to fix undefined

--- BlindAI/Behavior/Behavior.js
+++ BlindAI/Behavior/Behavior.js
@@ -2,12 +2,20 @@ class BlindAI extends SimpleAI{
   static _chase(start, dungeon){
     var xChange = dungeon.hero.location.x - start.x;
     var yChange = dungeon.hero.location.y - start.y;
+    var check = Utils.coordinateMorphs("all");
+    console.log(check);
     if (this._update(start, dungeon) == false) {
+      if (dungeon.map.cell[check.y][check.x].open == true) {
+        var munstor = Math.min(Math.sqrt(Math.pow(check, 2) + Math.pow(check, 2)));
+        var monstur = dungeon.map.cell[start.y][start.x].monster;
+        monstur = dungeon.map.cell[start.y][start.x].remove(monstur);
+        monstur = dungeon.map.cell[munstor.y][munstor.x].add(monstur);
+      }
       //use the a^2 + b^2 = c^2 method
       return Math.sqrt(Math.pow(xChange, 2) + Math.pow(yChange, 2));
     // } else {
     //   //use the a^2 + b^2 = c^2 method but take lowest value
-    //   return Math.sqrt(Math.pow(start, 2) + Math.pow(dungeon, 2));
+    //   return Math.sqrt(Math.pow(xChange, 2) + Math.pow(yChange, 2));
     }
   }
 }
