618d6a9467ad0346f9be6a9a147a725578754a9b
FuseRush <47150589+FuseRush@users.noreply.github.com>
2019-06-06 10:55:20 -0400

    need final function

--- BlindAI/Behavior/Behavior.js
+++ BlindAI/Behavior/Behavior.js
@@ -1,21 +1,49 @@
 class BlindAI extends SimpleAI{
-  static _chase(start, dungeon){
+
+  static _hypo(start, dungeon){
     var xChange = dungeon.hero.location.x - start.x;
     var yChange = dungeon.hero.location.y - start.y;
-    var check = Utils.coordinateMorphs("all");
-    console.log(check);
-    if (this._update(start, dungeon) == false) {
-      if (dungeon.map.cell[check.y][check.x].open == true) {
-        var munstor = Math.min(Math.sqrt(Math.pow(check, 2) + Math.pow(check, 2)));
-        var monstur = dungeon.map.cell[start.y][start.x].monster;
-        monstur = dungeon.map.cell[start.y][start.x].remove(monstur);
-        monstur = dungeon.map.cell[munstor.y][munstor.x].add(monstur);
-      }
-      //use the a^2 + b^2 = c^2 method
-      return Math.sqrt(Math.pow(xChange, 2) + Math.pow(yChange, 2));
-    // } else {
-    //   //use the a^2 + b^2 = c^2 method but take lowest value
-    //   return Math.sqrt(Math.pow(xChange, 2) + Math.pow(yChange, 2));
-    }
+
+    //use the a^2 + b^2 = c^2 method
+    return Math.sqrt(Math.pow(xChange, 2) + Math.pow(yChange, 2));
   }
+  static _area(start, dungeon){
+    var check = Utils.coordinateMorphs();
+    var retArr = [];
+    for (var i = 0; i < check.length; i++) {
+      var tst = {x: start.x + check[i].x, y: start.y + check[i].y};
+      if (dungeon.map.cell[tst.y][tst.x].open) {
+        retArr.push(tst);
+      }
+    }
+    return retArr;
+  }
+
+  static _getNear(start, dungeon){
+    var open = this._area(start, dungeon);
+    var hero = dungeon.hero.location
+    var best = Infinity;
+    var index = -1;
+    for (var i = 0; i < open.length; i++) {
+      var hypo = this._hypo(open[i], hero)
+      if (hypo < best) {
+        best = hypo;
+        index = i;
+      }
+    }
+    return open[index];
+  }
+
+//-----------------------------gross code------------------------------------//
+//   static _chase(start, dungeon){
+//
+//     var check = Utils.coordinateMorphs();
+//     if (this._update(start, dungeon) == false) {
+//       if (dungeon.map.cell[check.y][check.x].open == true) {
+//       }
+//     } else {
+//       //use the a^2 + b^2 = c^2 method but take lowest value
+//       return Math.sqrt(Math.pow(xChange, 2) + Math.pow(yChange, 2));
+//     }
+//   }
 }
