69cbcf557c38cc144419f1a56b6f6c18ba7f7527
DakotaAyer <47248767+DakotaAyer@users.noreply.github.com>
2019-06-06 09:02:04 -0400

    ///

--- BlindAI/Behavior/Behavior.js
+++ BlindAI/Behavior/Behavior.js
@@ -1,10 +1,8 @@
 class BlindAI extends SimpleAI{
   static _chase(start, dungeon, cords){
     if (this._update(start, dungeon) == false) {
-      //use the a^2 + b^2 = c^2 method
       return Math.sqrt(Math.pow(start, 2) + Math.pow(dungeon, 2));
     } else {
-      //use the a^2 + b^2 = c^2 method but take lowest value
 
     }
   }
